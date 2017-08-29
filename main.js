b552223979327bac31fbf75cf4bf2a88119cf486
Your Name <you@example.com>
2017-08-29 16:31:37 -0400

    even better style

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const template = `
 <div class="boxsize">
   <img src="${customers.results[i].picture.large}" alt="">
   <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
+  <p class="email">${customers.results[i].email}</p>
   <div>${customers.results[i].location.street}</div>
   <div>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</div>
   <div>${customers.results[i].cell}</div>
-  <p>${customers.results[i].id.value}</p>
+  <p class= "ssnum">${customers.results[i].id.value}</p>
 </div>
 
 `;
