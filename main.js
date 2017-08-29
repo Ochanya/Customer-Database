61575cdf94ed80af07c888b9735c1ba9537f03a9
Your Name <you@example.com>
2017-08-29 16:07:13 -0400

    fixed styling

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const template = `
 <div class="boxsize">
   <img src="${customers.results[i].picture.large}" alt="">
   <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
-  <p>${customers.results[i].location.street}</p>
-  <p>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
-  <p>${customers.results[i].cell}</p>
+  <div>${customers.results[i].location.street}</div>
+  <div>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</div>
+  <div>${customers.results[i].cell}</div>
   <p>${customers.results[i].id.value}</p>
 </div>
 
